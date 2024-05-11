36a1a24f1d4bc4066f613042ad6fdbb0a7680796
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-05-11 14:58:16 +0600

    balance  solved

--- js/deposite.js
+++ js/deposite.js
@@ -1,11 +1,18 @@
   document.getElementById('button-depo').addEventListener('click',function(){
 
    const maildepo = document.getElementById('email-depo')
-   const depoamount =maildepo.value;
-   console.log(depoamount)
+   const depoamountstring =maildepo.value;
+   const depoamount =parseFloat(depoamountstring);
+   console.log(typeof depoamount)
    const previousdepo = document.getElementById('total-depo')
-   const recentdepo = previousdepo.innerText;
-   previousdepo.innerText = depoamount;
+   const recentdepostring = previousdepo.innerText;
+   const recentdepo = parseFloat(recentdepostring)
+   previousdepo.innerText = depoamount +recentdepo
+   maildepo.value = '';
 
-
+   const balance =document.getElementById('total-balance')
+    const previousbalancestring = balance.innerText;
+    const previousbalance = parseFloat(previousbalancestring)
+    balance.innerText = depoamount + previousbalance;
+  
   })
